d07f5b4fb5356f3c1e0478d81cbb848f849c353c
Ghufran Kasho <ghufrankasho2@gmail.com>
2025-09-21 01:45:35 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware'=>'api','prefix'=>'auth'],function($router){
+Route::group(['prefix'=>'auth'],function($router){
     
     Route::post('/register',[AuthController::class,'register']);
     Route::post('/login',[AuthController::class,'login']);
