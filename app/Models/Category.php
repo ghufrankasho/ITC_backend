422a17ab4fbb910fe67e11cad99566fabee19c2c
Ghufran Kasho <ghufrankasho2@gmail.com>
2025-09-24 10:13:26 +0300

    commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -27,15 +27,36 @@ class Category extends Model
         static::deleting(function ($category) {
         if ($category->isForceDeleting()) {
             
+            $subcategories=$category->subcategories()->withTrashed()->get();
+            foreach($subcategories as $subcategory){
+                $products=$subcategory->products()->withTrashed()->get();
+                foreach( $products as $product){
+                    $product->forceDelete();
+                }
+            }
             // hard delete subcategories & products
             $category->subcategories()->withTrashed()->forceDelete();
         } else {
             // soft delete
+            $subcategories=$category->subcategories()->get();
+            foreach($subcategories as $subcategory){
+                $products=$subcategory->products()->get();
+                foreach( $products as $product){
+                    $product->delete();
+                }
+            }
             $category->subcategories()->delete();
         }
     });
             static::restoring(function ($category) {
             // restore subcategories
+             $subcategories=$category->subcategories()->withTrashed()->get();
+            foreach($subcategories as $subcategory){
+                $products=$subcategory->products()->withTrashed()->get();
+                foreach( $products as $product){
+                    $product->restore();
+                }
+            }
             $category->subcategories()->withTrashed()->restore();
     
         });
