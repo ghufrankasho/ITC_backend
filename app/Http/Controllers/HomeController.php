6250ae4c5053f58a131186be8eaba0ebe6d3b772
Ghufran Kasho <ghufrankasho2@gmail.com>
2025-10-13 14:38:08 +0300

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,12 +14,19 @@ class HomeController extends Controller
 {
     public function index(){
 
-        $sliders=Setting::where([['hide',0],['group','slider']])->get();
-        $categories=Category::where('hide',0)->with('subcategories')->get();
+        $sliders=Setting::where([['hide',0],['group','slider']])->latest()->get();
+        $categories=Category::where('hide',0)->with('subcategories')->latest()->get();
+        $products=Product::where('hide',0) ->latest()
+    ->take(6)
+    ->get();;
+        $news=News::where('hide',0)->latest()->get();
+        $settings=Setting::where([['hide',0],['group','footer']])->latest()->get();
         return [
-            'products'=>["data"],
+            'products'=> $products,
             'sliders'=>$sliders,
             'categories'=>$categories,
+            'news'=>$news,
+            'settings'=>$settings,
         ];
     }
 }
